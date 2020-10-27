a110d341f5cc9ee132a791bf55631e8024585fe6
Marvel Dev <marvellousalika48@gmail.com>
2020-10-27 06:33:22 +0100

    - add eventlistenrs

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-const form = document.querySelector('.input-form');
-const input = document.querySelector('#input');
-const nextQuestionButton = document.querySelector('#nextQtnButton');
-const repeatQtnButton = document.querySelector('#repeatButton');
-const showAnswerButton = document.querySelector('#answerButton');
-const firstValue = document.querySelector('#firstValue');
-const secondValue = document.querySelector('#secondValue');
-const correctMessage = document.querySelector('#correctMessage')
-const wrongMessage = document.querySelector('#wrongMessage')
-input.focus();
+const addEventListeners = () => {
+  const form = document.querySelector('.input-form');
+  const correctMessage = document.querySelector('#correctMessage')
+  const wrongMessage = document.querySelector('#wrongMessage')
+  const nextQuestionButton = document.querySelector('#nextQtnButton');
+  const repeatQtnButton = document.querySelector('#repeatButton');
+  const showAnswerButton = document.querySelector('#answerButton');
+  const input = document.querySelector('#input');
 
-const addNumbers = () => {
-  const result = parseFloat(firstValue.innerText) + parseFloat(secondValue.innerText);
-  return result;
-}
-
-const generateNumbers = () => {
-  const newFirstValue = Math.floor((Math.random() * 1000) + 1);
-  firstValue.innerText = newFirstValue;
-  const newSecondValue = Math.floor((Math.random() * 1000) + 1);
-  secondValue.innerText = newSecondValue;
-  input.focus();
-}
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (input.value == addNumbers()) {
-    correctMessage.style.display = 'block';
-    nextQuestionButton.style.display = 'inline-block';
-    showAnswerButton.style.display = 'none';
-    nextQuestionButton.focus();
-  } else {
-    wrongMessage.style.display = 'block';
-    repeatQtnButton.style.display = 'inline-block';
-    showAnswerButton.style.display = 'inline-block';
+  const addNumbers = () => {
+    const result = parseFloat(firstValue.innerText) + parseFloat(secondValue.innerText);
+    input.value = result;
+    return result;
   }
-})
+  
+  const generateNumbers = () => {
+    const newFirstValue = Math.floor((Math.random() * 1000) + 1);
+    firstValue.innerText = newFirstValue;
+    const newSecondValue = Math.floor((Math.random() * 1000) + 1);
+    secondValue.innerText = newSecondValue;
+    input.focus();
+  }
 
-nextQuestionButton.addEventListener('click', () => {
-  input.value = '';
-  generateNumbers();
-  correctMessage.style.display = 'none';
-  nextQuestionButton.style.display = 'none';
-})
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (input.value == addNumbers()) {
+      correctMessage.style.display = 'block';
+      nextQuestionButton.style.display = 'inline-block';
+      showAnswerButton.style.display = 'none';
+      nextQuestionButton.focus();
+    } else {
+      wrongMessage.style.display = 'block';
+      repeatQtnButton.style.display = 'inline-block';
+      showAnswerButton.style.display = 'inline-block';
+    }
+  })
 
-repeatQtnButton.addEventListener('click', () => {
-  wrongMessage.style.display = 'none';
-  repeatQtnButton.style.display = 'none';
-  showAnswerButton.style.display = 'inline-block';
-})
+  nextQuestionButton.addEventListener('click', () => {
+    input.value = '';
+    generateNumbers();
+    correctMessage.style.display = 'none';
+    nextQuestionButton.style.display = 'none';
+  })
 
-showAnswerButton.addEventListener('click', () => {
-  input.value = addNumbers();
-  wrongMessage.style.display = 'none';
-  repeatQtnButton.style.display = 'none';
-  showAnswerButton.style.display = 'none';
-  nextQuestionButton.style.display = 'inline-block';
-})
+  repeatQtnButton.addEventListener('click', () => {
+    wrongMessage.style.display = 'none';
+    repeatQtnButton.style.display = 'none';
+    showAnswerButton.style.display = 'inline-block';
+  })
+
+  showAnswerButton.addEventListener('click', () => {
+    input.value = addNumbers();
+    wrongMessage.style.display = 'none';
+    repeatQtnButton.style.display = 'none';
+    showAnswerButton.style.display = 'none';
+    nextQuestionButton.style.display = 'inline-block';
+  })
+}
+
+module.exports = { addEventListeners, addNumbers() };
